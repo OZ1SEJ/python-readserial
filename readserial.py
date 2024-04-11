c42e11b39217879e092f9a978b4532330c9d78f8
Steen Eiler Jørgensen <oz1sej@gmail.com>
2024-04-11 14:31:52 +0200

    Add files via upload

---
+++ readserial.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import serial,datetime,os
+
+#ser = serial.Serial('COM1',9600)		# Windows
+ser = serial.Serial('/dev/ttyUSB0',9600)	# Mac/Linux
+
+f   = open("serialdump.txt", "a+")
+
+while True:
+    s    = ser.readline()
+    now  = str(datetime.datetime.now())
+
+    line = str(now)
+    line += ","
+    #line = s.decode('utf-8').replace('\r\n','')
+    data  = s.decode(encoding='ascii',errors='ignore')
+    line += data
+
+    print(line.replace('\r\n',''))			# Prints output to screen
+    f.write(line)	# Appends output to file
+    f.flush()
